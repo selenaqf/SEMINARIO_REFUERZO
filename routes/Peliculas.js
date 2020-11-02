7910e1eb2ca13c491bdeaea81b49ca99478bfe32
selenaqf <selenaquintanillafuertes@gmail.com>
2020-11-02 16:26:19 -0400

    commit

--- routes/Peliculas.js
+++ routes/Peliculas.js
@@ -23,26 +23,73 @@ router.post("/peliculas", (req, res) => {
     });
 });
 
-router.get("/peliculas", (req, res) => {
-        PELICULAS.find({}).
-        exec((err, docs) => {
-        if (err) {
-            res.status(500).json({msn: "ERROR EN EL SERVIDOR"});
+router.get("/peliculas",(req, res) => 
+{
+    var filter = {};
+    var params=req.query;
+    var select = "";
+    var order = {};
+    if (params.Titulo_Pelicula != null)
+    {
+        var expresion = new RegExp(params.Titulo_Pelicula);
+        filter["Titulo_Pelicula"] =  expresion;
+
+    }
+
+    //filtro 
+    if(params.filters != null)
+    {
+        select = params.filters.replace(/,/g, " ");
+    }
+    if (params.order !=null)
+    {
+        var data = params.order.split(",");
+        var number =parseInt(data[1]);
+        order[data[0]] = number;
+    }
+    PELICULAS.find(filter).
+    select(select).
+    sort(order ).
+    exec((err,docs) =>
+    {
+        if (err) 
+        {
+            res.status(500).json({msn:"Error en el Servidor"});
             return;
-        }  
+        }
         res.status(200).json(docs);
         return;
     });
 });
- 
 
-router.put("/peliculas", (req, res) => {
-
+router.put("/peliculas", (req, res) => 
+{
+    var params = req.query;
+    var bodydata = req.body;
+    if(params.id == null)
+    {
+        res.status(300).json({msn: "El parametro ID es necesario"});
+        return;
+    }
+    var allowkeylist = ["Titulo_Pelicula", "Sinopsis", "Idioma"];  //especifica los datos que no deben actualizarce
+    var keys = Object.keys(bodydata);
+    var updateobjectdata = {};
+    for (var i = 0; i < keys.length; i++)
+    {
+        if (allowkeylist.indexOf(keys[i]) > -1)
+        {
+            updateobjectdata[keys[i]] = bodydata[keys[i]];
+        }
+    } // este es todo el codigo
+    PELICULAS.update({_id: params.id}, {$set: updateobjectdata}, (err, docs) =>
+    {
+    if (err)
+    {
+        res.status(500).json({msn: "Exite error en los datos"});
+        return;
+    }
+    res.status(200).json(docs);
+    });
 });
-
-router.delete("/peliculas", (req, res) => {
-
-});
-
 module.exports = router;
 
